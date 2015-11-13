942054d3f0203af4291d7d7aa2b19ba5515d7f14
lomila <liza.o.mansbach@gmail.com>
2015-11-13 11:35:18 -0500

    Update header.php

--- header.php
+++ header.php
@@ -2,10 +2,10 @@
 <nav class="centerme back_yellow noMargin">
 		<ul class="centerme">
 			<li class="inline bigText highlighted" ><a class ="spaceme" href="Index.php"> 	</a></li>
-			<li class="inline bigText "><a class ="spaceme" href="vendors.php">HISTORY</a></li>
-			<li class="inline bigText "><a class ="spaceme" href="performances.php">MEMBERS</a></li>
-			<li class="inline bigText "><a class ="spaceme" href="About.php">AUDITIONS</a></li>
-			<li class="inline bigText "><a class ="spaceme" href="Location.php">SHOWS</a></li>
+			<li class="inline bigText "><a class ="spaceme" href="history.php">HISTORY</a></li>
+			<li class="inline bigText "><a class ="spaceme" href="members.php">MEMBERS</a></li>
+			<li class="inline bigText "><a class ="spaceme" href="auditions.php">AUDITIONS</a></li>
+			<li class="inline bigText "><a class ="spaceme" href="performances.php">SHOWS</a></li>
 			<li class="inline bigText"><a href="contact.php" class="spaceme">CONTACT US</a></li>
 
 		</ul>
