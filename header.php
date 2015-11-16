05d081b8243662033d605843b44143e04ff9e6c1
aayushberi26 <aayush.beri26@gmail.com>
2015-11-16 14:47:05 -0500

    Update header.php

--- header.php
+++ header.php
@@ -1,13 +1,10 @@
-<?php 
-<nav class="centerme back_yellow noMargin">
-		<ul class="centerme">
-			<li class="inline bigText highlighted" ><a class ="spaceme" href="Index.php"> 	</a></li>
-			<li class="inline bigText "><a class ="spaceme" href="history.php">HISTORY</a></li>
-			<li class="inline bigText "><a class ="spaceme" href="members.php">MEMBERS</a></li>
-			<li class="inline bigText "><a class ="spaceme" href="auditions.php">AUDITIONS</a></li>
-			<li class="inline bigText "><a class ="spaceme" href="performances.php">SHOWS</a></li>
-			<li class="inline bigText"><a href="contact.php" class="spaceme">CONTACT US</a></li>
-
+ <nav class="nav centerme">
+		  <ul class = "centerme inline">
+              <li class="inline"><a  href="Index.php">HOME</a></li>
+              <li class="inline"><a  href="history.php">HISTORY</a></li>
+              <li class="inline"><a  href="members.php">MEMBERS</a></li>
+              <li class="inline"><a  href="auditions.php">AUDITIONS</a></li>
+              <li class="inline"><a  href="performances.php">SHOWS</a></li>
+              <li class="inline"><a  href="contact.php">CONTACT US</a></li>
 		</ul>
 	</nav>
-?>
