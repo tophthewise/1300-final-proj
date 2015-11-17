b4f66d73059d7289d7add1221ee5e4027cba3172
ERIC PORTER <ericporter@ERICs-MacBook-Pro.local>
2015-11-17 18:13:20 -0500

    Newest stuff

--- members.php
+++ members.php
@@ -6,13 +6,13 @@
 <body>
 	<?php include "header.php" ?>
 	<div class="icy_back">
-		<p >
+		<p>
 Emily McAllister '18 <br>
 New Rochelle, NY <br>
 Kumbaya chair <br>
 Soprano<br>
 Fun Fact: I hate Oreos  </p>
-		<img class = " pics" src="Images/IMG_1002.jpg" alt="Emily's picture">
+		<img class="bio_pics" class = " pics" src="Images/IMG_1002.jpg" alt="Emily's picture">
 	</div>
 	<div class="icy_back">
 		<p>Samantha Fischetti<br>
@@ -20,7 +20,7 @@ Staten Island, NY<br>
 Public Relations<br>
 Tenor<br>
 Fun Fact :in my spare time I like to frolic in water fountains in the upper NYC area</p>
-		<img src="" alt="">
+		<img class="bio_pics" src="Images/ShoshiShimmy.jpg" alt="">
 	</div>
 	<div class="icy_back">
 		<p>Jack Corcoran '19<br>
@@ -28,14 +28,14 @@ Crofton, MD<br>
 Pretty cool dude<br>
 Tenor<br>
 Fun fact:  has tasted the second hottest hot sauce in the world</p>
-		<img src="" alt="">
+		<img class="bio_pics" src="Images/jackspic.jpg" alt="">
 	</div>
 	<div class="icy_back">
 		<p>Ayobami Adebowale<br>
 Dix Hills, NY<br>
 Soprano <br>
 Fun Fact: Lived in 3 continents </p>
-		<img src="" alt="">
+		<img class="bio_pics" src="Images/" alt="">
 	</div>
 	<div class="icy_back">
 		<p>Stevie ferrara '16<br>
@@ -43,7 +43,7 @@ Point Pleasant, NJ<br>
 Vice President <br>
 Tenor<br>
 Fun fact: If you close your eyes and tilt your head to the left I look like Abe Lincoln.</p>
-		<img src="" alt="">
+		<img class="bio_pics" src="Images/SvendolynFerrari.JPG" alt="">
 	</div>
 	<div class="icy_back">
 		<p>Mei Poy '19<br>
@@ -51,7 +51,7 @@ Arlington Heights, IL<br>
 Musical Director<br>
 Soprano<br>
 Fun Fact: I recently taught myself morse code.</p>
-		<img src="" alt="">
+		<img class="bio_pics" src="Images/Chiraq.JPG" alt="">
 	</div>
 	<div class="icy_back">
 		<p>Grace Kaiser '19<br>
@@ -59,20 +59,20 @@ Staten Island, NY<br>
 Soprano<br>
 Fun Fact:Grace is really bad at thinking of fun facts
 </p>
-		<img src="" alt="">
+		<img class="bio_pics" src="Images/gracespic.JPG" alt="">
 	</div>
 	<div class="icy_back">
 		<p>Eric Porter '17<br>
 Poughkeepsie, NY<br>
 Baritone<br>
 Fun Fact: I love Web Development</p>
-		<img src="" alt="">
+		<img class="bio_pics" src="Images/zhuuwl.JPG" alt="">
 	</div>
 	<div class="icy_back">
 		<p>Brady Bunkelman '19<br>
 Chanhassen MN<br>
 Tenor1</p>
-		<img src="" alt="">
+		<img class="bio_pics" src="Images/unclerunkle.JPG" alt="">
 	</div>
 	<div class="icy_back">
 		<p>Carolyn Pascale '17<br>
@@ -80,7 +80,7 @@ Point Lookout,NY<br>
 President<br>
 Alto<br>
 Fun Fact: Has a twin </p>
-		<img src="" alt="">
+		<img class="bio_pics" src="Images/ButtStuff2.JPG" alt="">
 	</div>
 	<div class="icy_back">
 		<p>Jason Willam Gallant '18<br>
@@ -89,21 +89,21 @@ Fun Fact: Has a twin </p>
 			Tenor2( sorta base)<br>
 			Fun Fact: Is a brother in Alpha Sigma Phi
 </p>
-		<img src="" alt="">
+		<img class="bio_pics" src="Images/dad.JPG" alt="">
 	</div>
 	<div class="icy_back">
 		 <p>Carolyn Von Walter '16<br>
 West Palm Beach, FL<br>
 Fun Fact: Humans of Cornell University Photographer<br>
 Tenor</p>
-		<img src="" alt="">
+		<img class="bio_pics" src="Images/tetayvino.JPG" alt="">
 	</div>
 	<div class="icy_back">
 		<p>Elena Gupta '19<br>
 Ithaca, NY<rb>
 Alto<br>
 Fun Fact: I speak 4 Languages</p>
-		<img src="" alt="">
+		<img class="bio_pics" src="Images/TheDrunk.JPG" alt="">
 	</div>
 	<div class="icy_back">
 		<p>Delmar Fears '19<br>
@@ -111,7 +111,7 @@ Atlanta, GA<br>
 Alto<br>
 Fun Fact: Likes food and you
 </p>
-		<img src="" alt="">
+		<img class="bio_pics" src="Images/Delly.JPG" alt="">
 	</div>
 </body>
 </html>
