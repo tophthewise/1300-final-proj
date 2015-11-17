2c5fe5e7df155f3b216a994032b977c36561c957
ERIC PORTER <ericporter@dhcp-rhodes-875.eduroam.cornell.edu>
2015-11-17 11:59:21 -0500

    this muffucka needed to be in php form silly goose

---
+++ Index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<link rel="stylesheet" href="RedSolo.css">
+	</head>
+	<body>
+		<?php include "header.php" ?>
+        
+        <div id="content">
+		<img class =" pics" src="images/fullgroup.jpg" alt="A a full group photo from our photoshoot.">
+        </div>
+        
+	</body>
+</html>
