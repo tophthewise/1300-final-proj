2697cc0d0d9ce053737623791055c8f81a17cfcd
ERIC PORTER <ericporter@ERICs-MacBook-Pro.local>
2015-11-17 18:16:21 -0500

    new stuff again

---
+++ contact.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>	
+<html>
+<head>
+	<link rel="stylesheet" href="CSS/RedSolo.css">
+	<title>Contact</title>
+</head>
+<Body>
+	<?php include "header.php" ?>
+	<div class= "centerme icy_back">
+		<P>Wana try out of Red Solo??? Put in your info down below.<br> We look forward to hearing you!! </P>
+	<form action="post">
+		Name:<input type="text"><br>
+		year:<input type="text"><br>
+		Vocal Part:<input type="text"><br>
+		Favorite song:<input type="text"><br>
+		<input type="Submit" value="Lets Have a Party!!(Submit)">
+	</form>
+	<div>
+</Body>
+</html>
