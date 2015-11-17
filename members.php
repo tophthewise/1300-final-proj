14b9467c7eb2a0b54410b12c4368e8b12e19e2b1
ERIC PORTER <ericporter@dhcp-rhodes-875.eduroam.cornell.edu>
2015-11-17 13:40:50 -0500

    looks like a website now alrighty

--- members.php
+++ members.php
@@ -6,109 +6,109 @@
 <body>
 	<?php include "header.php" ?>
 	<div class="icy_back">
-		<p>
-Emily McAllister '18 
-New Rochelle, NY 
-Kumbaya chair 
-Soprano
+		<p >
+Emily McAllister '18 <br>
+New Rochelle, NY <br>
+Kumbaya chair <br>
+Soprano<br>
 Fun Fact: I hate Oreos  </p>
-		<img class = "pics" src="Images/IMG_1002.jpg" alt="Emily's picture">
+		<img class = " pics" src="Images/IMG_1002.jpg" alt="Emily's picture">
 	</div>
-	<div>
-		<p>Samantha Fischetti
-Staten Island, NY
-Public Relations
-Tenor
+	<div class="icy_back">
+		<p>Samantha Fischetti<br>
+Staten Island, NY<br>
+Public Relations<br>
+Tenor<br>
 Fun Fact :in my spare time I like to frolic in water fountains in the upper NYC area</p>
 		<img src="" alt="">
 	</div>
-	<div>
-		<p>Jack Corcoran '19
-Crofton, MD
-Pretty cool dude
-Tenor
+	<div class="icy_back">
+		<p>Jack Corcoran '19<br>
+Crofton, MD<br>
+Pretty cool dude<br>
+Tenor<br>
 Fun fact:  has tasted the second hottest hot sauce in the world</p>
 		<img src="" alt="">
 	</div>
-	<div>
-		<p>Ayobami Adebowale
-Dix Hills, NY
-Soprano 
+	<div class="icy_back">
+		<p>Ayobami Adebowale<br>
+Dix Hills, NY<br>
+Soprano <br>
 Fun Fact: Lived in 3 continents </p>
 		<img src="" alt="">
 	</div>
-	<div>
-		<p>Stevie ferrara '16
-point pleasant, NJ
-Vice President 
-Tenor
+	<div class="icy_back">
+		<p>Stevie ferrara '16<br>
+Point Pleasant, NJ<br>
+Vice President <br>
+Tenor<br>
 Fun fact: If you close your eyes and tilt your head to the left I look like Abe Lincoln.</p>
 		<img src="" alt="">
 	</div>
-	<div>
-		<p>Mei Poy '19
-Arlington Heights, IL
-Musical Director
-Soprano
+	<div class="icy_back">
+		<p>Mei Poy '19<br>
+Arlington Heights, IL<br>
+Musical Director<br>
+Soprano<br>
 Fun Fact: I recently taught myself morse code.</p>
 		<img src="" alt="">
 	</div>
-	<div>
-		<p>Grace Kaiser '19
-Staten Island, NY
-Soprano
+	<div class="icy_back">
+		<p>Grace Kaiser '19<br>
+Staten Island, NY<br>
+Soprano<br>
 Fun Fact:Grace is really bad at thinking of fun facts
 </p>
 		<img src="" alt="">
 	</div>
-	<div>
-		<p>Eric Porter '17
-Poughkeepsie, NY
-Baritone
+	<div class="icy_back">
+		<p>Eric Porter '17<br>
+Poughkeepsie, NY<br>
+Baritone<br>
 Fun Fact: I love Web Development</p>
 		<img src="" alt="">
 	</div>
-	<div>
-		<p>Brady Bunkelman '19
-Chanhassen MN
+	<div class="icy_back">
+		<p>Brady Bunkelman '19<br>
+Chanhassen MN<br>
 Tenor1</p>
 		<img src="" alt="">
 	</div>
-	<div>
-		<p>Carolyn Pascale '17
-Point Lookout,NY
-President
-Alto
+	<div class="icy_back">
+		<p>Carolyn Pascale '17<br>
+Point Lookout,NY<br>
+President<br>
+Alto<br>
 Fun Fact: Has a twin </p>
 		<img src="" alt="">
 	</div>
-	<div>
-		<p>Jason Willam Gallant '18
-CT
-Secretary
-Tenor2( sorta base)
-Fun Fact: Is a brother in Alpha Sigma Phi
+	<div class="icy_back">
+		<p>Jason Willam Gallant '18<br>
+			CT<br>
+			Secretary<br>
+			Tenor2( sorta base)<br>
+			Fun Fact: Is a brother in Alpha Sigma Phi
 </p>
 		<img src="" alt="">
 	</div>
-	<div>
-		 class="icy_back"<p>Carolyn Von Walter '16
-West Palm Beach, FL
-Fun Fact: Humans of Cornell University Photographer
+	<div class="icy_back">
+		 <p>Carolyn Von Walter '16<br>
+West Palm Beach, FL<br>
+Fun Fact: Humans of Cornell University Photographer<br>
 Tenor</p>
 		<img src="" alt="">
 	</div>
-	<div>
-		<p>Elena Gupta '19
-Ithaca, NG
-Alto
+	<div class="icy_back">
+		<p>Elena Gupta '19<br>
+Ithaca, NY<rb>
+Alto<br>
 Fun Fact: I speak 4 Languages</p>
 		<img src="" alt="">
 	</div>
-	<div>
-		<p>Delmar Fears '19
-Atlanta, GA
-Alto
+	<div class="icy_back">
+		<p>Delmar Fears '19<br>
+Atlanta, GA<br>
+Alto<br>
 Fun Fact: Likes food and you
 </p>
 		<img src="" alt="">
