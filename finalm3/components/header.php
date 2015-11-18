cc2f32e1617d9bc0517e7b18d46a93115e64dd5a
aayushberi26 <aayushberi@dhcp-uris-2347.eduroam.cornell.edu>
2015-11-18 13:11:50 -0500

    This is another version of the website

---
+++ finalm3/components/header.php
@@ -0,0 +1,8 @@
+ <nav class="nav centerme">
+		  <ul class = "centerme ">
+              <li class="inline"><a  href="../Index.php">HOME</a></li>
+              <li class="inline"><a  href="../pages/members.php">MEMBERS</a></li>
+              <li class="inline"><a  href="../pages/performances.php">SHOWS</a></li>
+              <li class="inline"><a  href="../pages/contact.php">CONTACT US</a></li>
+		</ul>
+	</nav>
---
+++ finalm3/pages/members.php
@@ -0,0 +1,119 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<link rel="stylesheet" href="../CSS/RedSolo.css">
+<title>Members</title>
+</head>
+<body>
+	<?php include "../components/header.php" ?>
+	<div class="icy_back">
+		<p>
+Emily McAllister '18 <br>
+New Rochelle, NY <br>
+Kumbaya chair <br>
+Soprano<br>
+Fun Fact: I hate Oreos  
+        </p>
+		<img class="bio_pics" src="../Images/IMG_1002.jpg" alt="Emily's picture">
+	</div>
+	<div class="icy_back">
+		<p>Samantha Fischetti<br>
+Staten Island, NY<br>
+Public Relations<br>
+Tenor<br>
+Fun Fact :in my spare time I like to frolic in water fountains in the upper NYC area</p>
+		<img class="bio_pics" src="../Images/ShoshiShimmy.JPG" alt="Samantha's picture">
+	</div>
+	<div class="icy_back">
+		<p>Jack Corcoran '19<br>
+Crofton, MD<br>
+Pretty cool dude<br>
+Tenor<br>
+Fun fact:  has tasted the second hottest hot sauce in the world</p>
+		<img class="bio_pics" src="../Images/jackspic.JPG" alt="Jack's Picture">
+	</div>
+	<div class="icy_back">
+		<p>Ayobami Adebowale<br>
+Dix Hills, NY<br>
+Soprano <br>
+Fun Fact: Lived in 3 continents </p>
+	</div>
+	<div class="icy_back">
+		<p>Stevie ferrara '16<br>
+Point Pleasant, NJ<br>
+Vice President <br>
+Tenor<br>
+Fun fact: If you close your eyes and tilt your head to the left I look like Abe Lincoln.</p>
+		<img class="bio_pics" src="../Images/SvendolynFerrari.JPG" alt="Stevie's Picture">
+	</div>
+	<div class="icy_back">
+		<p>Mei Poy '19<br>
+Arlington Heights, IL<br>
+Musical Director<br>
+Soprano<br>
+Fun Fact: I recently taught myself morse code.</p>
+		<img class="bio_pics" src="../Images/Chiraq.JPG" alt="Mei's Picture">
+	</div>
+	<div class="icy_back">
+		<p>Grace Kaiser '19<br>
+Staten Island, NY<br>
+Soprano<br>
+Fun Fact:Grace is really bad at thinking of fun facts
+</p>
+		<img class="bio_pics" src="../Images/gracespic.JPG" alt="Grace's Picture">
+	</div>
+	<div class="icy_back">
+		<p>Eric Porter '17<br>
+Poughkeepsie, NY<br>
+Baritone<br>
+Fun Fact: I love Web Development</p>
+		<img class="bio_pics" src="../Images/zhuuwl.JPG" alt="Eric's Picture">
+	</div>
+	<div class="icy_back">
+		<p>Brady Bunkelman '19<br>
+Chanhassen MN<br>
+Tenor1</p>
+		<img class="bio_pics" src="../Images/unclerunkle.JPG" alt="Brady's Picture">
+	</div>
+	<div class="icy_back">
+		<p>Carolyn Pascale '17<br>
+Point Lookout,NY<br>
+President<br>
+Alto<br>
+Fun Fact: Has a twin </p>
+		<img class="bio_pics" src="../Images/ButtStuff2.JPG" alt="Carolyn's Picture">
+	</div>
+	<div class="icy_back">
+		<p>Jason Willam Gallant '18<br>
+			CT<br>
+			Secretary<br>
+			Tenor2( sorta base)<br>
+			Fun Fact: Is a brother in Alpha Sigma Phi
+</p>
+		<img class="bio_pics" src="../Images/Dad.JPG" alt="Jason's Picture">
+	</div>
+	<div class="icy_back">
+		 <p>Carolyn Von Walter '16<br>
+West Palm Beach, FL<br>
+Fun Fact: Humans of Cornell University Photographer<br>
+Tenor</p>
+		<img class="bio_pics" src="../Images/tetayvino.JPG" alt="Carolyn's Picture">
+	</div>
+	<div class="icy_back">
+		<p>Elena Gupta '19<br>
+Ithaca, NY<br>
+Alto<br>
+Fun Fact: I speak 4 Languages</p>
+		<img class="bio_pics" src="../Images/TheDrunk.JPG" alt="Elena's Picture">
+	</div>
+	<div class="icy_back">
+		<p>Delmar Fears '19<br>
+Atlanta, GA<br>
+Alto<br>
+Fun Fact: Likes food and you
+</p>
+		<img class="bio_pics" src="../Images/Delly.JPG" alt="Delmar's Picture">
+     
+	</div>
+</body>
+</html>
