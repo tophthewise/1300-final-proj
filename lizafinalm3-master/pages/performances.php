95c0e411c20437d69bee288f31475976f0176ebf
ERIC PORTER <ericporter@dhcp-clara-275.eduroam.cornell.edu>
2015-11-30 19:55:26 -0500

    lizas site is fire

---
+++ lizafinalm3-master/pages/performances.php
@@ -0,0 +1,43 @@
+<!DOCTYPE HTML>
+<html>
+<Head>
+	<title>Shows</title>
+	<link rel="stylesheet" href="../CSS/RedSolo.css">
+    <link href='https://fonts.googleapis.com/css?family=Dosis' rel='stylesheet' type='text/css'>
+</Head>
+<Body>
+	<?php include "../components/header.php" ?>
+	<br><br>
+	<Table class="table">
+		<tr>
+			<td><h2> Perfomance </h2></td>
+			<td><h2> Location </h2></td>
+            <td><h2>Time</h2></td>
+		</tr>
+		<tr>
+			<td> Fall 2015: Under Pressure</td>
+			<td>Statler</td>
+            <td>8pm Nov 21 </td>
+		</tr>
+		<tr>
+			<td>TBD</td>
+			<td>TBD</td>
+            <td>TBD</td>
+		</tr>
+		<tr>
+			<td>TBD</td>
+			<td>TBD</td>
+            <td>TBD</td>
+
+		</tr>
+		<tr>
+			<td>TBD</td>
+			<td>TBD</td>
+            <td>TBD</td>
+
+		</tr>
+		
+	</Table>
+ 
+</Body>
+</html>
