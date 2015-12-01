0257e2eb30afa20c6e4d506f9a39e515f5beb0a8
ERIC PORTER <ericporter@dhcp-mcfad-891.redrover.cornell.edu>
2015-11-30 23:15:35 -0500

    newest file

--- finalm4/components/header.php
+++ finalm4/components/header.php
@@ -1,7 +1,7 @@
 <nav class="nav centerme fatpad">
 		  <ul class = " centerme ">
 		  	  <li class="inline2"><img class =" logo_pic" src="../Images/redsolologo.png" alt=""></li>
-              <li class="inline spaceme"><a  href="../Index.php">HOME</a></li>
+              <li class="inline spaceme"><a  href="../index.php">HOME</a></li>
               <li class="inline spaceme"><a  href="members.php">MEMBERS</a></li>
               <li class="inline spaceme"><a  href="performances.php">SHOWS</a></li>
               <!--<li class="inline spaceme"><a  href="pages/media.php">MEDIA</a></li> -->
--- finalm4/pages/members.php
+++ finalm4/pages/members.php
@@ -9,7 +9,7 @@
 	<?php include "../components/header.php" ?>
 	<div class="icy_back columns">
 
-		<img class="bio_pics" src="../Images/IMG_1002.jpg" alt="Emily's picture">
+		<img class="bio_pics" src="../images/IMG_1002.jpg" alt="Emily's picture">
 		<p class="inline2">
 Emily McAllister '18 <br>
 New Rochelle, NY <br>
@@ -18,7 +18,7 @@ Soprano<br>
 Fun Fact: I hate Oreos  
         </p>
 	<br>
-	<img class="bio_pics" src="../Images/ShoshiShimmy.JPG" alt="Samantha's picture">
+	<img class="bio_pics" src="../images/ShoshiShimmy.JPG" alt="Samantha's picture">
 		<p class= "inline2">Samantha Fischetti<br>
 Staten Island, NY<br>
 Public Relations<br>
@@ -26,20 +26,20 @@ Tenor<br>
 Fun Fact :I like to frolic in  water fountains in the upper NYC area.</p>
 		
 	<br>
-	<img class="bio_pics" src="../Images/jackspic.JPG" alt="Jack's Picture">
+	<img class="bio_pics" src="../images/jackspic.JPG" alt="Jack's Picture">
 		<p class= "inline2">Jack Corcoran '19<br>
 Crofton, MD<br>
 Pretty cool dude<br>
 Tenor<br>
 Fun fact: He has tasted the second hottest hot sauce in the world.</p>
 		<br>
-		<img src="../Images/Bami.jpg" alt="" class="bio_pics">
+		<img src="../images/Bami.jpg" alt="" class="bio_pics">
 		<p class= "inline2">Ayobami Adebowale<br>
 Dix Hills, NY<br>
 Soprano <br>
 Fun Fact: Lived in 3 continents </p>
     <br>
-	<img class="bio_pics" src="../Images/SvendolynFerrari.JPG" alt="Stevie's Picture">
+	<img class="bio_pics" src="../images/SvendolynFerrari.JPG" alt="Stevie's Picture">
 		<p class= "inline2">Stevie ferrara '16<br>
 Point Pleasant, NJ<br>
 Vice President <br>
@@ -47,7 +47,7 @@ Tenor<br>
 Fun fact: If you close your eyes and tilt your head to the left I look like Abe Lincoln.</p>
 		
 	<br>
-<img class="bio_pics" src="../Images/Chiraq.JPG" alt="Mei's Picture">
+<img class="bio_pics" src="../images/Chiraq.JPG" alt="Mei's Picture">
 		<p class= "inline2">Mei Poy '19<br>
 Arlington Heights, IL<br>
 Musical Director<br>
@@ -55,7 +55,7 @@ Soprano<br>
 Fun Fact: I recently taught myself morse code.</p>
 		
 	<br>
-<img class="bio_pics" src="../Images/gracespic.JPG" alt="Grace's Picture">
+<img class="bio_pics" src="../images/gracespic.JPG" alt="Grace's Picture">
 		<p class= "inline2">Grace Kaiser '19<br>
 Staten Island, NY<br>
 Soprano<br>
@@ -63,7 +63,7 @@ Fun Fact:Grace is really bad at thinking of fun facts
 </p>
 		
 	<br>
-	<img class="bio_pics" src="../Images/zhuuwl.JPG" alt="Eric's Picture">
+	<img class="bio_pics" src="../images/zhuuwl.JPG" alt="Eric's Picture">
 		<p class= "inline2">Eric Porter '17<br>
 Poughkeepsie, NY<br>
 Baritone<br>
@@ -71,7 +71,7 @@ Fun Fact: I love Web Development</p>
 		
 	<br>
 	
-	<img class="bio_pics" src="../Images/unclerunkle.JPG" alt="Brady's Picture">
+	<img class="bio_pics" src="../images/unclerunkle.JPG" alt="Brady's Picture">
 		<p class= "inline2">Brady Bunkelman '19<br>
 Chanhassen MN<br>
 Tenor1</p>
@@ -79,7 +79,7 @@ Tenor1</p>
 
 	<br>
 	
-			<img class="bio_pics" src="../Images/ButtStuff2.JPG" alt="Carolyn's Picture">
+			<img class="bio_pics" src="../images/ButtStuff2.JPG" alt="Carolyn's Picture">
 		<p class= "inline2">Carolyn Pascale '17<br>
 Point Lookout,NY<br>
 President<br>
@@ -88,7 +88,7 @@ Fun Fact: Has a twin </p>
 	
 	<br>
 	
-		<img class="bio_pics" src="../Images/Dad.JPG" alt="Jason's Picture">
+		<img class="bio_pics" src="../images/Dad.JPG" alt="Jason's Picture">
 		<p class= "inline2">Jason Willam Gallant '18<br>
 			CT<br>
 			Secretary<br>
@@ -98,7 +98,7 @@ Fun Fact: Has a twin </p>
 		
 	<br>
 	
-				<img class="bio_pics" src="../Images/tetayvino.JPG" alt="Carolyn's Picture">
+				<img class="bio_pics" src="../images/tetayvino.JPG" alt="Carolyn's Picture">
 		 <p class= "inline2">Carolyn Von Walter '16<br>
 West Palm Beach, FL<br>
 Tenor <br>
@@ -107,7 +107,7 @@ Fun Fact: Humans of Cornell University Photographer<br>
 
 	<br>
 	
-		<img class="bio_pics" src="../Images/TheDrunk.JPG" alt="Elena's Picture">
+		<img class="bio_pics" src="../images/TheDrunk.JPG" alt="Elena's Picture">
 		<p class= "inline2">Elena Gupta '19<br>
 Ithaca, NY<br>
 Alto<br>
@@ -115,7 +115,7 @@ Fun Fact: I speak 4 Languages</p>
 
 	<br>
 	
-<img class="bio_pics" src="../Images/Delly.JPG" alt="Delmar's Picture">
+<img class="bio_pics" src="../images/Delly.JPG" alt="Delmar's Picture">
 		<p class= "inline2">Delmar Fears '19<br>
 Atlanta, GA<br>
 Alto<br>
