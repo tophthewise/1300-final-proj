59f3b4a53b93b590235d3bf6b1a77a3f96bcb508
ERIC PORTER <ericporter@ERICs-MacBook-Pro.local>
2015-12-09 23:57:58 -0500

    finalm5

---
+++ finalm5/pages/media.php
@@ -0,0 +1,59 @@
+<!DOCTYPE HTML>
+<html>
+<Head>
+	<title>Shows</title>
+	<link rel="stylesheet" href="../css/redsolo.css">
+    <link href='https://fonts.googleapis.com/css?family=Dosis' rel='stylesheet' type='text/css'>
+</Head>
+<Body>
+	<?php 
+		include "../components/header.php"; 
+	?>
+	<br>
+
+       <ul class="icy_back">
+
+	<span class="head"> Songs (Covers)</span>
+	<li class="nav2 ">
+		Lean on Cover
+     <audio controls>
+  	<source src="../music/Lean_On.m4a" type="audio/mpeg"  />
+  Your browser does not support the audio tag.
+	</audio>
+	</li>
+	
+	<li class="nav2 ">
+		Landslide Cover
+	<audio controls>
+  <source class ="icy_back" src="../music/Landslide.m4a" type="audio/mpeg"/>
+  Your browser does not support the audio tag.
+	</audio>
+	</li>
+
+<li class="nav2 ">
+	Shark in the water Cover
+	<audio controls>
+  <source src="../music/Shark_In_The_Water.m4a" type="audio/mpeg"/>
+  Your browser does not support the audio tag.
+	</audio>
+	</li>
+            
+	<li class="nav2 ">
+		Under Pressure Cover 
+	<audio controls>
+  <source src="../music/Under_Pressure_Recording.m4a" type="audio/mpeg"/>
+  Your browser does not support the audio tag.
+	</audio>
+	</li>
+
+	</ul>
+<div class="icy_back">
+	<span class="  head">Videos</span>
+	<ul>
+		<li class="inline">	<iframe class="  vids" src="https://www.youtube.com/embed/8wKM_9GBiXg"  allowfullscreen></iframe></li>
+		<li class="inline"> <video class=" vids" controls> <source src="../movies/dayinthelife.m4v" type="video/mp4"/> Your browser does not support the video tag.</video></li>
+	</ul>
+</div>
+
+</Body>
+</html>
